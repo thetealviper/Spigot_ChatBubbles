a4a53194aa760fde543e366cdbdc7e270f2ec8c7
TheTealViper <thetealviper@gmail.com>
2022-04-09 09:40:04 -0700

    [1.18.1.a.9] Mode 5 Prefix List + Prefix Removal Bug Fix

--- src/me/TheTealViper/chatbubbles/implentations/ChatListenerPrototype.java
+++ src/me/TheTealViper/chatbubbles/implentations/ChatListenerPrototype.java
@@ -1,5 +1,7 @@
 package me.TheTealViper.chatbubbles.implentations;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -47,14 +49,22 @@ public class ChatListenerPrototype {
 			plugin.handleFour(e.getMessage(), e.getPlayer());
 			break;
 		case 5:
-			String prefix = plugin.getConfig().getString("ConfigFive_Prefix_Characters");
-			if(e.getMessage().startsWith(prefix)) {
+			// Find out if message starts with config.yml prefixes, and if so which one
+			List<String> prefixes = plugin.getConfig().getStringList("ConfigFive_Prefix_Characters");
+			String foundPrefix = "";
+			for(String prefix : prefixes) {
+				if(!foundPrefix.equals("")) continue;
+				if(e.getMessage().startsWith(prefix)) {
+					foundPrefix = prefix;
+				}
+			}
+			//If message starts with a prefix
+			if(!foundPrefix.equals("")) {
 				//If player has manually toggled to disable the hologram functionality
 				if(!plugin.togglePF.getBoolean(e.getPlayer().getUniqueId().toString())) return;
 				if(!plugin.getConfig().getBoolean("ChatBubble_Send_Original_Message")) //This MUST be handled here. If handled by manual player.chat() leads to infinite recursion onPlayerChatEvent
 					e.setCancelled(true);
-				else
-					e.setMessage(e.getMessage().substring(prefix.length())); //This is uniquely necessary to this config mode 5
+				e.setMessage(e.getMessage().substring(foundPrefix.length())); //This is uniquely necessary to this config mode 5
 				plugin.handleFive(e.getMessage(), e.getPlayer());
 			}
 			break;
